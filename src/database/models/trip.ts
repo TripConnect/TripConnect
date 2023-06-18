666ad7f9c1240eb27c5a1aff1c23786f138ad2ac
Kinder <nguyenductrong12a1@gmail.com>
2023-06-18 22:33:12 +0700

    Refactor code

--- src/database/models/trip.ts
+++ src/database/models/trip.ts
@@ -2,26 +2,26 @@ import { DataTypes } from 'sequelize';
 
 const { sequelize } = require('./index');
 
-const UserCredential = sequelize.define('Trip', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  created_by: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  created_at: {
-    type: DataTypes.DATE,
-    allowNull: false
-  },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
+const Trip = sequelize.define('Trip', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    created_by: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
 }, {
-  tableName: 'trip',
-  timestamps: false,
+    tableName: 'trip',
+    timestamps: false,
 });
 
-export default UserCredential;
+export default Trip;
