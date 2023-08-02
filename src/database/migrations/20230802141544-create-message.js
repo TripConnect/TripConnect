d85bbd14fc51ea0716f055664e1edb0dd6e1200b
Kinder <nguyenductrong12a1@gmail.com>
2023-08-02 23:03:12 +0700

    Add migration to implement chatting feature

---
+++ src/database/migrations/20230802141544-create-message.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'message',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        content: {
+          allowNull: false,
+          type: Sequelize.STRING
+        },
+        state: {
+          allowNull: false,
+          type: Sequelize.ENUM('sent', 'seen', 'revoked')
+        },
+        created_at: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        from_user_id: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        to_user_id: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      },
+      {
+        timestamps: false // Disable createdAt and updatedAt fields
+      }
+    );
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('message');
+  }
+};
---
+++ src/database/models/message.ts
@@ -0,0 +1,37 @@
+import { DataTypes } from 'sequelize';
+
+import db from '.';
+
+const Message = db.sequelize.define('Message', {
+  trip_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  from_user_id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  to_user_id: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  state: {
+    type: DataTypes.ENUM,
+    values: ['sent', 'seen', 'revoked'],
+  }
+
+}, {
+  tableName: 'message',
+  timestamps: false,
+});
+
+export default Message;
